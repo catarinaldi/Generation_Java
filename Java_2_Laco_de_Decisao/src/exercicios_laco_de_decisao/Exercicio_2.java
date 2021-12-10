eb3cbef5e650b41b1723c208ff77dc3e3ff8f0c2
Catarina Rinaldi <catarinarinaldi@hotmail.com>
2021-12-10 13:47:57 -0300

    Exercícios Java 2 - Generation Brasil

--- Java_2_Laco_de_Decisao/src/exercicios_laco_de_decisao/Exercicio_2.java
+++ Java_2_Laco_de_Decisao/src/exercicios_laco_de_decisao/Exercicio_2.java
@@ -11,29 +11,28 @@ public class Exercicio_2 {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		int n1, n2, n3, cresc, lugar1, lugar2, lugar3;
+		int n1, n2, n3;
 		
 		System.out.println("Escreva três números:");
 		n1 = scanner.nextInt();
 		n2 = scanner.nextInt();
 		n3 = scanner.nextInt();
 		
-		if (n1 >= n2 && n1 >= n3) {
-			cresc = n1;
-		}	
-		
-		if (n2 >= n3 && n2 >= n1) {
-			cresc = n2;
-		}		
-		
-		if (n3 >= n1 && n3 >= n2) {
-			cresc = n3;
+		if (n1 >= n2 && n2 >= n3) {
+			System.out.println("Números em ordem crescente: " + n3 + " | " + n2 + " | " + n1);	
+		}	else if (n1 >= n3 && n3 >= n2) {
+			System.out.println("Números em ordem crescente: " + n2 + " | " + n3 + " | " + n1);
+		}	else if (n2 >= n1 && n1 >= n3) {
+			System.out.println("Números em ordem crescente: " + n3 + " | " + n1 + " | " + n2);	
+		}	else if (n2 >= n3 && n3 >= n1) {
+			System.out.println("Números em ordem crescente: " + n1 + " | " + n3 + " | " + n2);
+		}	else if (n3 >= n2 && n2 >= n1) {
+			System.out.println("Números em ordem crescente: " + n1 + " | " + n2 + " | " + n3);	
+		}	else if (n3 >= n1 && n1 >= n2) {
+			System.out.println("Números em ordem crescente: " + n2 + " | " + n1 + " | " + n3);
 		}
-		
-		System.out.println("Números em ordem crescente:" + lugar1 + lugar2 + lugar3);
-		
+	
 		scanner.close();
-		
 		
 	}
 }
