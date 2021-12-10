1da9a2a3755c7708a31ca931165ea28295489d6c
Catarina Rinaldi <catarinarinaldi@hotmail.com>
2021-12-09 21:43:00 -0300

    Exercícios Java 2 - Generation Brasil

---
+++ Java_2_Laco_de_Decisao/src/exercicios_laco_de_decisao/Exercicio_1.java
@@ -0,0 +1,41 @@
+package exercicios_laco_de_decisao;
+
+import java.util.Scanner;
+
+public class Exercicio_1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num1, num2, num3, maiorNum = 0;
+		
+		System.out.println("Escreva um número:");
+		num1 = scanner.nextInt();
+		
+		System.out.println("Escreva um número:");
+		num2 = scanner.nextInt();
+		
+		System.out.println("Escreva um número:");
+		num3 = scanner.nextInt();
+		
+		if (num1 >= num2 && num1 >= num3) {
+			maiorNum = num1;
+		}	
+		
+		if (num2 >= num3 && num2 >= num1) {
+			maiorNum = num2;
+		}		
+		
+		if (num3 >= num1 && num3 >= num2) {
+			maiorNum = num3;
+		}
+		
+		System.out.println("O maior número é " + maiorNum + "!!!");
+		
+		scanner.close();
+		
+
+
+	}
+}
