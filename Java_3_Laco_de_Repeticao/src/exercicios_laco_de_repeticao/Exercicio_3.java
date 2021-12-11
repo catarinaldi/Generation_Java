7dbe46ac0b4ed8910f139bd268c66d0d514fdefa
Catarina Rinaldi <catarinarinaldi@hotmail.com>
2021-12-10 22:41:07 -0300

    Exercícios Java 3 - Generation Brasil

--- Java_3_Laco_de_Repeticao/src/exercicios_laco_de_repeticao/Exercicio_3.java
+++ Java_3_Laco_de_Repeticao/src/exercicios_laco_de_repeticao/Exercicio_3.java
@@ -10,20 +10,20 @@ public class Exercicio_3 {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		int idade, i = 0, i2 = 0;
+		int idade = 0, i = 0, i2 = 0;
 		
-		System.out.println("Escreva sua idade");
-		idade = scanner.nextInt();
-		
-		while (idade < -99) {
-			if (idade <= 21) {
+		while (idade != -99) {
+			System.out.println("Escreva sua idade");
+			idade = scanner.nextInt();
+			
+			if (idade <= 21 && idade >= 0) {
 				i++;
 			} else if (idade >= 50) {
-					i2++;
+				i2++;
 				}
-			}
+		}
 
-		System.out.printf ("Idade");
+		System.out.printf ("Quantidade de pessoas com menos de 21: %d\nQuantidade de pessoas com mais de 50: %d", i, i2);
 	}
 
 }
